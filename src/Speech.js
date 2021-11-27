d7a0323b29bb45daae5f1b645029e1eb8f40b281
Elliot Rancie <erparker00@gmail.com>
2021-11-27 18:08:15 +1100

    new hirozons

--- src/Speech.js
+++ src/Speech.js
@@ -16,14 +16,15 @@ export default function Speech() {
   const [isListening, setIsListening ] = useState(false)
   const [text, setText ] = useState(null)
 
-  recognition.onstart = () => console.log('onstart')    //define what happens everytime recognition has started listening
-  recognition.onresult = event => {                     //define what happens everytime recognition produces a result event
-    const transcript = Array.from(event.results)        //build a transcript string
+  recognition.onstart = () => console.log('onstart')
+
+  recognition.onresult = event => {
+    const transcript = Array.from(event.results)
     .map(result => result[0])
     .map(result => result.transcript)
     .join('')
     setText(transcript); console.log(transcript);
-    recognition.onerror = event => console.log(event.error) //log err
+    recognition.onerror = event => console.log(event.error) 
   }
 
   useEffect(()=>{
@@ -31,35 +32,31 @@ export default function Speech() {
   }, [isListening])
 
   const handleListen = () => {
-    if(isListening){                                    // IF listening then define another start within onend callback
+    if(isListening){                                   
       recognition.start(); console.log('start');
-      recognition.onend = () => {                       // callback records instructions on what to do when 'end' event occurs
+      recognition.onend = () => {                       
         console.log('onend')
         recognition.start(); console.log('restart');
       }
-    }else {                                             // IF NOT listening define no restart onend
+    }else {
       recognition.stop(); console.log('stop');
       recognition.onend =()=> console.log('onend') 
     }
   }
   return (
     <div className='container'>
+
       <div className='box'>
-        <button className='btn btn-primary btn-lg p-5 m-5' onClick={()=> setIsListening(prevState => !prevState)}>{!isListening ? 'Listen' : 'Stop'}</button>
+        <button className='btn btn-primary btn-lg p-5 m-5' 
+                onClick={()=> setIsListening(prevState => !prevState)}>
+          {!isListening ? 'Listen' : 'Stop'}
+        </button>
+
         {isListening ? <div className='mb-5'><i>Listening</i></div> : <div className='mb-5'><i>Not Listening</i></div>}
       </div>
+
       <Prompt speechString={text} />
 
     </div>
   )
 }
-
-      // speech result logging
-      // console.log(event.results)
-      // console.log(event.results[0])
-      // console.log(event.results[0].transcript)//undefined?
-
-
-      // The onend property of the SpeechRecognition interface represents an event handler 
-      // that will run when the speech recognition service has disconnected 
-      // (when the 'end' event fires.)
