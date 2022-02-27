35f726594d226fa5fb762025b8e3dd33b745c5ae
Elliot Rancie <erparker00@gmail.com>
2022-02-28 00:20:12 +1100

    state raised to speech comp - working

--- src/Prompt.js
+++ src/Prompt.js
@@ -1,85 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import OpenAI from 'openai-api';
-import {} from 'dotenv/config';
-import Response from './Response';
+// import React, { useEffect, useState } from 'react'
+// import OpenAI from 'openai-api';
+// import {} from 'dotenv/config';
+// import Response from './Response';
 
-export default function Prompt(props) {
+// export default function Prompt(props) {
 
-  const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
-  const openai = new OpenAI(OPENAI_API_KEY);
+//   const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+//   const openai = new OpenAI(OPENAI_API_KEY);
   
+//   const [promptState, setPromptState] = useState({
+//     prompt: props.speechString,
+//     completion: '',
+//   })
+// //TODO: fix onType - typed additions to the text area must reflect in the parent's text
 
-  //TODO: TRY LIFTING THIS STATE UP  (Or redux?)
-  const [promptState, setPromptState] = useState({
-    prompt: props.speechString,
-    completion: '',
-  })
+//             //   props.onType(promptState.prompt) 
 
-  useEffect(()=> { // if props from parent change - reset state
-    setPromptState({prompt: props.speechString})
-  }, [props.speechString]) 
+//   useEffect(()=> { // if props from parent change - reset state
+//     setPromptState({prompt: props.speechString})
+//   }, [props.speechString]) 
 
+//   // useEffect(()=> { //if state from child changes - send to parent (lifting state up?)
+//   //   props.onType(promptState.prompt)
+//   // }, [promptState.prompt]) 
 
-  //typing changes to child state - send to parent 
-  // useEffect(()=> { 
-  //   props.onType(promptState.prompt)
-  // }, [promptState.prompt]) 
-  
-  //^^^^^^^^^^^^^^^^^^^^
-  //works or typing but makes parent prev state undefined on sendprompt
-  //leave out and parent prev state in tact on sendprompt
+//   //handle add click by sending completion back to parent
+//   // const handleAdd =(completion)=>{
+//   //   props.onComplete(completion)
+//   // }
+//   const handleAdd =()=>{
+//     props.onComplete(promptState.completion) //why does this clear child state data??
+//     // props.onComplete(promptState.prompt)
+//   }
 
-  const handleAdd =()=>{
-    props.onComplete(promptState.completion) 
-  }//why does this clear child state data??
+//   const handleChange = (e) => {
+//     setPromptState(prev => ({...prev, prompt: e.target.value}))
 
-  const handleChange = (e) => {
-    setPromptState({prompt: e.target.value})
-  }
-  // const handleChange = (event)=>{ 
-  //   // props.onType(promptState.prompt);
-  //   const { name, value } = event.target
-  //   setPromptState((preValue)=>{  
-  //     return {
-  //       ...preValue,
-  //       [name]: value
-  //     }
-  //   })
-  //   console.log(name); console.log(value);
-  // }
+//   }
+//   // const handleChange = (event)=>{ 
+//   //   // props.onType(promptState.prompt);
+//   //   const { name, value } = event.target
+//   //   setPromptState((preValue)=>{  
+//   //     return {
+//   //       ...preValue,
+//   //       [name]: value
+//   //     }
+//   //   })
+//   //   console.log(name); console.log(value);
+//   // }
 
-  async function sendPrompt() {
-    const gptResponse = await openai.complete({
-        engine: 'ada', //davinci-instruct-beta
-        maxTokens: 64,
-        prompt: promptState.prompt
-    });
-    setPromptState({completion: gptResponse.data.choices[0].text})
-  }
+//   async function sendPrompt() {
+//     const gptResponse = await openai.complete({
+//         engine: 'ada', //davinci-instruct-beta
+//         maxTokens: 64,
+//         prompt: promptState.prompt
+//     });
+//     setPromptState({completion: gptResponse.data.choices[0].text})
+//   }
 
-  return (
-    <div className='container'>
-      <textarea className='in' 
-                type="text" 
-                name='prompt' 
-                onChange={handleChange} 
-                value={promptState.prompt}>
-      </textarea>
+//   return (
+//     <div className='container'>
+//       <textarea className='in' 
+//                 type="text" 
+//                 name='prompt' 
+//                 onChange={handleChange} 
+//                 value={promptState.prompt}>
+//       </textarea>
 
-      <button className='btn btn-warning btn-lg p-5 m-5' 
-              type='button' 
-              onClick={sendPrompt} >
-        Complete
-      </button>
+//       <button className='btn btn-warning btn-lg p-5 m-5' 
+//               type='button' 
+//               onClick={sendPrompt} >
+//         Complete
+//       </button>
       
-      <Response completion={promptState.completion} />
+//       <Response completion={promptState.completion} />
 
-      <button className='btn btn-success btn-lg p-5 m-5' 
-              type='button' 
-              onClick={handleAdd} >
-        Add
-      </button>
+//       <button className='btn btn-success btn-lg p-5 m-5' 
+//               type='button' 
+//               onClick={handleAdd} >
+//         Add
+//       </button>
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
--- src/Speech.js
+++ src/Speech.js
@@ -1,7 +1,12 @@
 //shout out to Darwin Tech (https://www.youtube.com/watch?v=U2g--_TDYj4)
 //and Mohan Raj for inspo with this component (https://www.section.io/engineering-education/speech-recognition-in-javascript/)
 import React, { useEffect, useState } from 'react'
-import Prompt from './Prompt'
+import OpenAI from 'openai-api';
+import {} from 'dotenv/config';
+import Response from './Response';
+
+const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+const openai = new OpenAI(OPENAI_API_KEY);
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 console.log(SpeechRecognition)
@@ -15,8 +20,9 @@ recognition.lang = 'en-US'
 export default function Speech() {
   //state
   const [isListening, setIsListening ] = useState(false)
-  const [text, setText ] = useState(null)
+  const [text, setText ] = useState('')
   const [transcript, setTranscript ] = useState(null)
+  const [completion, setCompletion ] = useState(null)
 
   //recognition
   recognition.onstart = () => console.log('onstart')
@@ -34,6 +40,15 @@ export default function Speech() {
     handleListen(); console.log('handleListen()');
   }, [isListening])
 
+  async function sendPrompt() {
+    const gptResponse = await openai.complete({
+        engine: 'ada', //davinci-instruct-beta
+        maxTokens: 64,
+        prompt: text
+    });
+    setCompletion(gptResponse.data.choices[0].text)
+    console.log(`gpt: ${gptResponse.data.choices[0].text}`)
+  }
   //handlers
   const handleListen = () => {
     if(isListening){                                   
@@ -46,18 +61,11 @@ export default function Speech() {
       recognition.stop(); console.log('stop'); 
       recognition.onend =()=> {console.log('onend') 
         setText(prevText => prevText +' '+ transcript)
+        setTranscript('')
       }
     }
   }
-  //**handle onComplete - include completion string to make new text string
-  const handleComplete = (completion) => {
-    setText(prevState => {
-      return prevState +' '+ completion
-    })
-  }
-  const handleType = (prompt) => {
-    setText(prompt)
-  }
+
   return (
     <div className='container'>
 
@@ -69,12 +77,45 @@ export default function Speech() {
 
         {isListening ? <div className='mb-5'><i>Listening</i></div> : <div className='mb-5'><i>Not Listening</i></div>}
       </div>
+      {/* <h3></h3> */}
+      <div style={{height: '100px'}}>
+        {isListening ? <p>{transcript}</p> : null}
+      </div>
 
-      <p style={{height: '100px'}}>{transcript}</p>
+      <textarea className='in' 
+                type="text" 
+                name='text' 
+                onChange={(e)=>setText(e.target.value)} 
+                value={text}>
+                {/* value={isListening ? transcript : text}> */}
+      </textarea>
 
-      <Prompt speechString={text} 
-              onComplete={handleComplete}  
-              onType={handleType} />
+      <button className='btn btn-warning btn-lg p-5 m-5' 
+              type='button' 
+              onClick={sendPrompt} >
+        Complete
+      </button>
+      
+      <Response completion={completion} />
+
+      <button className='btn btn-success btn-lg p-5 m-5' 
+              type='button' 
+              onClick={()=>setText(prev=>prev+' '+completion)} >
+        Add
+      </button>
+
     </div>
   )
 }
+
+
+
+  // const handleChange = (event)=>{ 
+  //   setText(event.target.value)
+  // }
+
+  // const handleComplete = () => {
+  //   setText(prevState => {
+  //     return prevState +' '+ completion
+  //   })
+  // }
